615b5a0436f1eb833a1291ea8db7635b31934e64
hann0t <hansolivas1@gmail.com>
2021-12-07 21:59:50 -0500

    App stateful.

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,6 @@
 import React, { useState } from 'react';
 
-import CreateTodo from '../CreateTodo';
-import TodoContainer from '../TodoContainer';
-import TodoItem from '../TodoItem';
-import Filter from '../Filter';
-
-import './App.css';
-
-import toggleThemeIcon from '../img/icon-sun.svg';
+import AppUI from './AppUI';
 
 let todosContent = [
   {
@@ -58,44 +51,16 @@ const App = () => {
   }
 
   return (
-    <React.Fragment>
-      <header className='main-header'>
-        <div className='container'>
-          <h1>TODO</h1>
-          <img
-            src={toggleThemeIcon}
-            alt='toggle icon'
-            className='toggle-theme__icon'
-          />
-        </div>
-      </header>
-      <section className='middle-section'>
-        <div className='container'>
-          <CreateTodo setTodos={setTodos} todos={todos} />
-          <TodoContainer
-            completedTodos={completedTodos}
-            leftTodos={leftTodos}
-            todos={todos}
-            setTodos={setTodos}
-          >
-            {todosToShow.map((todo) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onCompleteTodo={onCompleteTodo}
-                onDeleteTodo={onDeleteTodo}
-              />
-            ))}
-          </TodoContainer>
-        </div>
-      </section>
-      <footer className='main-footer'>
-        <div className='container'>
-          <Filter todos={todos} setTodosFilter={setTodosFilter} />
-        </div>
-      </footer>
-    </React.Fragment>
+    <AppUI
+      completedTodos={completedTodos}
+      leftTodos={leftTodos}
+      todosToShow={todosToShow}
+      todos={todos}
+      setTodos={setTodos}
+      onCompleteTodo={onCompleteTodo}
+      onDeleteTodo={onDeleteTodo}
+      setTodosFilter={setTodosFilter}
+    />
   );
 };
 
