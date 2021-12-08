930bbac0787582b7ac7eb12fec7a64f5bb33811a
hann0t <hansolivas1@gmail.com>
2021-12-07 21:59:59 -0500

    deleteCompletedTodos fixed.

--- src/TodoContainer/index.js
+++ src/TodoContainer/index.js
@@ -9,9 +9,14 @@ const TodoContainer = ({
   todos,
   setTodos,
 }) => {
-  const deleteCompletedTodos = (completedTodos) => {
+  const deleteCompletedTodos = () => {
     let newTodos = [...todos];
-    completedTodos.forEach((todo) => newTodos.splice(todo, 1));
+    completedTodos.forEach((completedTodo) => {
+      let todoIndex = newTodos.findIndex(
+        (todo) => todo.text === completedTodo.text,
+      );
+      newTodos.splice(todoIndex, 1);
+    });
     setTodos(newTodos);
   };
 
@@ -20,12 +25,7 @@ const TodoContainer = ({
       {children}
       <footer className='todo-container__footer'>
         <span className='todo-left'>{leftTodos} items left</span>
-        <span
-          onClick={() => {
-            deleteCompletedTodos(completedTodos);
-          }}
-          className='todo-clear'
-        >
+        <span onClick={deleteCompletedTodos} className='todo-clear'>
           Clear Completed
         </span>
       </footer>
